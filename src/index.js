fa4057e711f12a37709c3b42338f5005eed582e5
smaximov <s.b.maximov@gmail.com>
2016-04-27 01:38:05 +1000

    Add `Lordown` class stub

---
+++ src/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Markdown to LORCODE converter.
+ */
+class Lordown {
+  /**
+   * Create a new instance of {@link Lordown}.
+   */
+  constructor() {
+  }
+
+  /**
+   * Convert Markdown to LORCODE.
+   *
+   * @param {string} input
+   * @return {string}
+   */
+  convert(input) {
+    return input
+  }
+}
+
+module.exports = Lordown
