05d0d5e0e7e1a2b0c9791d6c3dd77ee7b091e360
smaximov <s.b.maximov@gmail.com>
2016-04-28 00:16:58 +1000

    Userscript is now arguably usable

--- userscript/index.js
+++ userscript/index.js
@@ -1,4 +1,56 @@
 const Lordown = require('../lib')
 const lordown = new Lordown
 
-console.log(lordown.convert('hello, @smaximov!'))
+function handle(element, event, handler) {
+  element.addEventListener(event, handler, false)
+}
+
+function choice(...ids) {
+  for (let id of ids) {
+    let result = document.getElementById(id)
+    if (result !== null) return result
+  }
+
+  return null
+}
+
+function clone(node, updateAttr={}) {
+  const result = node.cloneNode()
+  for (let attr in updateAttr) {
+    result.setAttribute(attr, updateAttr[attr])
+  }
+
+  return result
+}
+
+function init(form) {
+  if (form === null) return
+
+  const msg = choice('msg', 'form_msg')
+  if (msg === null) return
+
+  const markdownMsg = clone(msg, {
+    id: 'lordown-msg',
+    name: 'lordown-msg'
+  })
+
+  msg.parentNode.insertBefore(markdownMsg, msg)
+  msg.style.display = 'none'
+
+  const convert = () => {
+    msg.value = lordown.convert(markdownMsg.value)
+  }
+
+  // Async previews use XHR instead of submit
+  const previewButton = form.querySelector('button[type=button][name=preview]')
+  if (previewButton !== null) {
+    // Use `mousedown` instead of `click` since it's fired first
+    handle(previewButton, 'mousedown', convert)
+  }
+
+  handle(form, 'submit', convert)
+}
+
+handle(window, 'load', () => {
+  init(choice('commentForm', 'messageForm'))
+})
