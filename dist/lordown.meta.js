cb13899874148448e475f850d88e0db85c5131be
smaximov <s.b.maximov@gmail.com>
2016-05-11 02:41:31 +1000

    Bump version

--- dist/lordown.meta.js
+++ dist/lordown.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name lordown
 // @description Markdown to LORCODE converter
-// @version 0.2.0
+// @version 0.3.0
 // @grant none
 // @namespace https://www.linux.org.ru
 // @include https://www.linux.org.ru/*
